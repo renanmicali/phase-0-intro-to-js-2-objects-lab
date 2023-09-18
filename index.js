68b81f80a60cead78d06f7e4ba12f63f98c128b8
Renan Kozuka Micali <renanmicali@hotmail.com>
2023-09-18 15:12:58 +1000

    Emploeey Updates

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee = {
+    name : "Carlos",
+    streetAddress : "Isabela Street"
+}
+
+function updateEmployeeWithKeyAndValue() {
+    return {
+        ...employee,
+        ["name"]: "Sam",
+        streetAddress : "11 Broadway"
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+    
+    employee.streetAddress = "12 Broadway";
+    return employee
+
+}
+
+function deleteFromEmployeeByKey() {
+
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee
+
+}
+
+function destructivelyDeleteFromEmployeeByKey() {
+    delete employee.name;
+    return employee
+}
